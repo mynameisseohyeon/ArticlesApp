32d912a9d365e5925ce9a6ad3ccadd461cf5917f
seohyeon <uimi3606@gmail.com>
2023-07-23 17:54:09 +0900

    BUG adb 명령어 실행이 안됨, strapi 게시글 작성 오류 발견

--- screens/ArticlesScreen.tsx
+++ screens/ArticlesScreen.tsx
@@ -1,14 +1,23 @@
-//게시글 목록 조회
-
 import React from 'react';
-import {Text, View} from 'react-native';
+import {ActivityIndicator, StyleSheet} from 'react-native';
+import {useQuery} from 'react-query';
+import {getArticles} from '../api/articles';
+import Articles from '../components/Articles';
 
 function ArticlesScreen() {
-  return (
-    <View>
-      <Text>Articles</Text>
-    </View>
-  );
+  const {data} = useQuery('articles', getArticles);
+
+  if (!data) {
+    return <ActivityIndicator size="large" style={styles.spinner} />;
+  }
+
+  return <Articles articles={data} />;
 }
+
+const styles = StyleSheet.create({
+  spinner: {
+    flex: 1,
+  },
+});
 
 export default ArticlesScreen;
